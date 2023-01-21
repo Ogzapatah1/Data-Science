43643d41de672d4eb1b795c80aa65131095305ee
Ogzapatah1 <121770574+Ogzapatah1@users.noreply.github.com>
2023-01-21 11:16:02 -0500

    Add files via upload

--- Data Science Process Iris.py
+++ Data Science Process Iris.py
@@ -21,6 +21,8 @@ print(iris_data.target)
 print(iris_data.target_names)
 
 # Create a dataframe from the Iris data
+#I am commenting some of the dataframe convertions of the bunch type
+#because I dont know exactlty what the commented lines are doing...
 df = pd.DataFrame(iris_data['data'], columns=iris_data['feature_names'])
 #df['target'] = iris_data['target']
 #df['target_name'] = iris_data['target_names']
@@ -48,8 +50,8 @@ import matplotlib.pyplot as plt
 # Step 4: Preprocess the data and split into train and test
 
 # Split the data into train and test sets
-#I am getting an error defining X
-#maybe i dont need to get the X and y values from the dataframe df?
+#I am getting an error defining X with the dataframe objects, so i am using the
+#original bunch type
 
 from sklearn.model_selection import train_test_split
 
@@ -57,6 +59,12 @@ X = iris_data.data
 y = iris_data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+#print the shapes of these new data variables
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
 
 # Standardize the data
 from sklearn.preprocessing import StandardScaler
@@ -73,9 +81,21 @@ model.fit(X_train, y_train)
 
 # Evaluate the model
 accuracy = model.score(X_test, y_test)
-print("Accuracy:", accuracy)
+print("Accuracy of Logistic:", accuracy)
+
+#Train and evaluate a K-near model
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=11)
+knn.fit(X_train, y_train)
+
+#Evaluate second model accuracy
+accuracy2 = knn.score(X_test, y_test)
+print("Accuracy of K-near:", accuracy2)
 
 # Step 6: Communicate the results
 
 # Report the findings and accuracy of the model
 print("The logistic regression model accurately predicted the type of Iris plant with an accuracy of", accuracy)
+
+print("The K-near model accurately predicted the type of Iris plant with an accuracy of", accuracy2)
+
